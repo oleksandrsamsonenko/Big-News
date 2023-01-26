c8a0576c7047e1e1208c3c28ce3c8917783782f9
kilimanki <www.slipperstrong@gmail.com>
2023-01-26 19:04:20 +0100

    dk-update-markup

--- src/js/markup.js
+++ src/js/markup.js
@@ -1,41 +1,53 @@
 import axios from 'axios';
 const newsList = document.querySelector('.list-news');
-const NEW_IRL =
-  'https://newsapi.org/v2/everything?' +
-  `q= sport` +
-  '&from=2022-12-26&sortBy=publishedAt&apiKey=cfc62234939b43f5b960da3e6ec231b2';
-const getFetch = async e => {
+const inputEl = document.querySelector('.nav-input');
+const NEW_URL = 'https://newsapi.org/v2/everything?';
+const API_KEY = 'cfc62234939b43f5b960da3e6ec231b2';
+async function getFetch(searchValue) {
   try {
-    const response = await axios.get(`${NEW_IRL}`);
+    const response = await axios.get(`${NEW_URL}`, {
+      params: {
+        q: searchValue,
+        from: '2022-12-26',
+        apiKey: API_KEY,
+        sortBy: 'publishedAt',
+        safesearch: true,
+        page: 1,
+        pageSize: 40,
+      },
+    });
     console.log(response.data.articles);
     const atributes = response.data.articles;
-    atributes.map(item => {
-      createMarkup(item);
-    });
+    return atributes;
   } catch (error) {
     console.log(error);
   }
-};
-getFetch();
-function createMarkup(e) {
-  const date = new Date(e.publishedAt);
-  const day = String(date.getDate()).padStart(2, '0');
-  const month = String(date.getMonth() + 1).padStart(2, '0');
+}
 
-  const getTime = `${day} / ${month} / ${date.getFullYear()} `;
-  const markup = ` <li>
-<div class="images"><img src="${e.urlToImage}" alt="" width="288px" height="395px" />
-<button class="img-btn">Add to favorite</button></div>
-        
-        <h2>${e.title}</h2>
-        
-        
-        <p>${e.description}</p>
+function createMarkup(arr) {
+  const markup = arr
+    .map(item => {
+      const date = new Date(item.publishedAt);
+      const day = String(date.getDate()).padStart(2, '0');
+      const month = String(date.getMonth() + 1).padStart(2, '0');
+
+      const getTime = `${day} / ${month} / ${date.getFullYear()} `;
+      return ` <li>
+<div class="images"><img src="${item.urlToImage}" alt="" width="288px" height="395px" />
+<button class="img-btn">Add to favorite</button></div>     
+        <h2 class="description-title">${item.title}</h2>
+        <p>${item.description}</p>
         <div class="info-more">
           <p class="date">${getTime}</p>
           <a href="">Read more</a>
         </div>
 
       </li>`;
-  newsList.insertAdjacentHTML('beforeend', markup);
+    })
+    .join('');
+  newsList.innerHTML = markup;
+}
+inputEl.addEventListener('input', handleInput);
+function handleInput(e) {
+  getFetch(e.target.value.trim()).then(data => createMarkup(data));
 }
